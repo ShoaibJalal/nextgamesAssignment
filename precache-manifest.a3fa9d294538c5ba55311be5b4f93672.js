942879ed49bc1fe6abdebebbf8aae3ff872f33ed
ShoaibJalal <syedshoaibjalal@outlook.com>
2019-11-07 17:49:02 +0200

    Updates

---
+++ precache-manifest.a3fa9d294538c5ba55311be5b4f93672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb6701c8201c4d5df72dc773115f2c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7464a34d19b4c10ef2c",
+    "url": "/static/css/2.b9dc5a1e.chunk.css"
+  },
+  {
+    "revision": "48154813ffe50476d5a1",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c7464a34d19b4c10ef2c",
+    "url": "/static/js/2.ee54213f.chunk.js"
+  },
+  {
+    "revision": "48154813ffe50476d5a1",
+    "url": "/static/js/main.6cbbf2e0.chunk.js"
+  },
+  {
+    "revision": "f34b1ae6203ce331e894",
+    "url": "/static/js/runtime-main.60ccd23c.js"
+  }
+]);
